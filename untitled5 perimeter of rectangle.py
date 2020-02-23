b9d746cee7c85a3976c5c459220ba950ccefdd0d
NIKHIL CHAND <61118151+gnikhilchand@users.noreply.github.com>
2020-02-23 20:49:22 +0530

    Add files via upload

---
+++ untitled5 perimeter of rectangle.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 23 20:47:40 2020
+
+@author: Nikhil
+"""
+
+#perimeter of rectangle
+length=int(input("length: "))
+width=int(input("width: "))
+perimeter=2*(length*width)
+print("perimeter_of_rectangle:",perimeter)
