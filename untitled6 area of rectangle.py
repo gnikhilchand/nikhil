57fafeb2698d4decda65fa6b5ef32337112afb7d
NIKHIL CHAND <61118151+gnikhilchand@users.noreply.github.com>
2020-02-23 20:51:18 +0530

    Add files via upload

---
+++ untitled6 area of rectangle.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 23 20:49:52 2020
+
+@author: Nikhil
+"""
+
+#area of rectangle
+length=int(input("length: "))
+width=int(input("width: "))
+area=length*width
+print("area_of_rectangle:",area)
