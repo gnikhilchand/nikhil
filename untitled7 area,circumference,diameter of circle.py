86730d0e84b5b259685a7806e93b7273a73b6982
NIKHIL CHAND <61118151+gnikhilchand@users.noreply.github.com>
2020-02-23 21:18:27 +0530

    Add files via upload

---
+++ untitled7 area,circumference,diameter of circle.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 23 21:17:03 2020
+
+@author: Nikhil
+"""
+
+#area of circle
+radius=int(input("radius"))
+area=3.14*radius*radius
+print("area_of circle:",area)
+
+#circumference of circle
+radius=int(input("radius"))
+circumference=2*3.14*radius
+print("circumference_of_circle:",circumference)
+
+#diameter of circle
+radius=int(input("radius"))
+diameter=2*radius
+print("diameter_of_circle:",diameter)
