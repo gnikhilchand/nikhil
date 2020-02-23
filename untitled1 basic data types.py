b6b6ef84fd1fa324c675a889d8c2950efa0e5345
NIKHIL CHAND <61118151+gnikhilchand@users.noreply.github.com>
2020-02-23 20:38:34 +0530

    Add files via upload

---
+++ untitled1 basic data types.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 23 20:33:42 2020
+
+@author: Nikhil
+"""
+
+#int
+print(10)
+print(-20)
+
+#float
+x=1.0
+print(x)
+print(-2.0)
+
+#complex
+x=complex(0,2)
+y=complex(2,1)
+print(x)
+print(y)
+
+#bool
+print(True)
+x=False
+print(x)
