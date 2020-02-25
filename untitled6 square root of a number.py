60bcbaa568bd53886e7d0f7abf755d0219534c83
NIKHIL CHAND <61118151+gnikhilchand@users.noreply.github.com>
2020-02-25 12:54:53 +0530

    Add files via upload

---
+++ untitled6 square root of a number.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 25 12:44:42 2020
+
+@author: Nikhil
+"""
+
+# square root of a number
+import math
+number = float(input(" Please Enter any number: "))
+square_root = math.sqrt(number)
+print("The Square Root of a Given Number: ",square_root)
