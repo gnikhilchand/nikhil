938040f1a76eeeaa58bea60229254272deb0240c
NIKHIL CHAND <61118151+gnikhilchand@users.noreply.github.com>
2020-02-25 12:58:33 +0530

    Add files via upload

---
+++ untitled7 angles of a triangle.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 25 12:56:26 2020
+
+@author: Nikhil
+"""
+
+# angles of triangle
+a=float(input("first angle: "))
+b=float(input("second angle: "))
+c=180-(a+b)
+print("third amgle= ",c)
