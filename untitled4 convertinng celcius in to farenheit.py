f175b50870781adc6a16e63711754b6105f26f07
NIKHIL CHAND <61118151+gnikhilchand@users.noreply.github.com>
2020-02-25 12:25:45 +0530

    Add files via upload

---
+++ untitled4 convertinng celcius in to farenheit.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 25 12:16:12 2020
+
+@author: Nikhil
+"""
+
+ #convert celcius in to farenheit 
+celcius=int(input("please enter temperature in celcius: "))    
+farenheit=(celcius*1.8)+32
+print("tempetature_in_farenheit: ",farenheit)
+
+
+
+
