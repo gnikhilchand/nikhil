39af7cd54c3b3faed19e82a8cd23e3349d2d5e61
NIKHIL CHAND <61118151+gnikhilchand@users.noreply.github.com>
2020-02-25 12:13:12 +0530

    Add files via upload

---
+++ untitled3 converting cm in to m, km.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 25 12:11:56 2020
+
+@author: Nikhil
+"""
+
+#converting cm into meter and km
+cm=1
+meter = cm / 100
+kilometer = cm / 100000 
+print("Length in meter = " ,meter , "m"); 
+print("Length in Kilometer = ",kilometer , "km");
