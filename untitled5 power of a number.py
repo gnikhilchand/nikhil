5a14999cae7262d50d1978ffafdbd2ab407cf4d6
NIKHIL CHAND <61118151+gnikhilchand@users.noreply.github.com>
2020-02-25 12:43:56 +0530

    Add files via upload

---
+++ untitled5 power of a number.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 25 12:26:45 2020
+
+@author: Nikhil
+"""
+
+#power of a number
+
+number = int(input(" Please Enter any Positive Integer : "))
+exponent = int(input(" Please Enter Exponent Value : "))
+power = pow(number,exponent)  
+print("The Result of Power of given number: " ,power)
+
+
+
