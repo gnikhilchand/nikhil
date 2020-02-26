9e80e5db9206bc2774e27892b5b8710dd8809f15
NIKHIL CHAND <61118151+gnikhilchand@users.noreply.github.com>
2020-02-26 11:48:28 +0530

    Add files via upload

---
+++ untitled10 area of triangle with base and height.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 25 14:23:40 2020
+
+@author: Nikhil
+"""
+
+
+# area of a triangle with base and height
+base = float(input('Please Enter the Base of a Triangle: '))
+height = float(input('Please Enter the Height of a Triangle: '))
+area = (base * height) / 2
+print("The Area of a Triangle using base" ,base, "and height" ,height, "=" ,area)
+
+
+
---
+++ untitled11 area of equilaterial triangle.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 25 14:33:11 2020
+
+@author: Nikhil
+"""
+
+# area of equilateral triangle
+import math
+side = float(input('Enter the Length of the Triangle: '))
+Area = (math.sqrt(3)/ 4)*(side * side)
+print(" Area of Equilateral Triangle: = " ,Area)
