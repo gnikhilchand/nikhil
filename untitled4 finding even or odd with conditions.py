f4c2f4f94e6058d8a90998801846ba6c8229ffee
NIKHIL CHAND <61118151+gnikhilchand@users.noreply.github.com>
2020-03-03 11:26:10 +0530

    Add files via upload

---
+++ untitled4 finding even or odd with conditions.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  3 11:23:11 2020
+
+@author: Nikhil
+"""
+
+
+#finding even or odd with conditions
+number = int(input("please enter the number: "))
+
+if number/2:
+    print("the number ia an even number")
+else:
+    print("the numbber is an odd number")
