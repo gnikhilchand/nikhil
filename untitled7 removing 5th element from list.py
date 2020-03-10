ad460091ee533d3842da42cede44e05c5aff9dbe
NIKHIL CHAND <61118151+gnikhilchand@users.noreply.github.com>
2020-03-10 16:40:49 +0530

    Add files via upload

---
+++ untitled7 removing 5th element from list.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 16:38:22 2020
+
+@author: Nikhil
+"""
+
+## creating list removing last element,5th element
+
+lis1 = [10,20,30,40,50]
+
+lis1.pop()
+
+lis1.pop(4)
+
+lis1.remove(50)
+
+print(lis1)
