97c17b4b409ff9405084918706bc20f81f8d4504
NIKHIL CHAND <61118151+gnikhilchand@users.noreply.github.com>
2020-03-10 16:00:03 +0530

    Add files via upload

---
+++ untitled3 creating dictionary with tuples.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 12:16:12 2020
+
+@author: Nikhil
+"""
+
+# creating a list with tuples
+
+a = (1,2)
+b = (2,3)
+c = (3,4)
+
+type(a)
+
+dict1 = {1:a,2:b,3:c}
+#      (or)
+dict2 = {'a':a,'b':b,'c':c}
+
+
