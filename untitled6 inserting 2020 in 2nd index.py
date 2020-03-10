0a55e589ade2dd88f632edffab0232497bb0b972
NIKHIL CHAND <61118151+gnikhilchand@users.noreply.github.com>
2020-03-10 16:26:25 +0530

    Add files via upload

---
+++ untitled6 inserting 2020 in 2nd index.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 16:20:52 2020
+
+@author: Nikhil
+"""
+
+# innserting 2020 in 2nd index
+
+my_list = [22,33,44,55,66]
+my_list.insert((2),2020)
+print(my_list)
