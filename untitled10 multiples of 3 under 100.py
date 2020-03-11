d82aca5831dcfdfc02aac83df041dfb8f8ff8d8d
NIKHIL CHAND <61118151+gnikhilchand@users.noreply.github.com>
2020-03-11 10:38:12 +0530

    Add files via upload

---
+++ untitled10 multiples of 3 under 100.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 17:17:50 2020
+
+@author: Nikhil
+"""
+
+# multiples of 3 under 100
+
+for i in range(0,100):
+    if i % 3 == 0:
+        print(i)
